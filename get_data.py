a1f09b2fc52495f9b03a285bc7b78a1610ed8899
Linus Heinzl <linus.heinzl@student.hpi.de>
2019-12-22 23:56:04 +0100

    has sorted top list now

--- get_data.py
+++ get_data.py
@@ -49,6 +49,7 @@ class RestaurantInfo:
     def _get_number_of_reviews(self, restaurant_tag: Tag) -> int:
         review_tag: Tag = restaurant_tag.find_all("span", {"class": re.compile("userReviewCount")})[0]
         review_name: str = review_tag.string
+        review_name = review_name.replace(",", "")
         return int(review_name.split(" ")[0])
 
     def _get_rank(self, restaurant_tag) -> int:
@@ -69,14 +70,14 @@ class RestaurantInfo:
         return displayed_name
 
 
+def get_top_restaurants_url(town: str, api_key: str) -> str:
+    google_search_api_response: dict = get_google_search_results(town, api_key)
+    all_results: List[dict] = google_search_api_response["items"]
+    general_restaurant_entry: dict = next(filter(lambda entry: "BEST Restaurants in".lower() in entry["title"].lower(), all_results))
+    assert general_restaurant_entry is not None
+    return general_restaurant_entry["link"]
 
-
-
-
-
-
-
-def get_google_search_result(town: str, api_key: str) -> dict:
+def get_google_search_results(town: str, api_key: str) -> dict:
     if town.lower() == "antibes":
         file: Path = Path("antibes_google_response.json")
         cached_result: dict = json.load(file.open())
@@ -88,30 +89,37 @@ def get_google_search_result(town: str, api_key: str) -> dict:
     api_url = f"https://www.googleapis.com/customsearch/v1?q={query}&cx={custom_engine}&key={api_key}"
     return requests.get(api_url).json()
 
-
-def get_top_restaurants_url(town: str, api_key: str) -> str:
-    google_search_api_response: dict = get_google_search_result(town, api_key)
-    all_results: List[dict] = google_search_api_response["items"]
-    general_restaurant_entry: dict = next(filter(lambda entry: "BEST Restaurants in".lower() in entry["title"].lower(), all_results))
-    assert general_restaurant_entry is not None
-    return general_restaurant_entry["link"]
-
-
-def get_top_restaurants(url: str) -> List[RestaurantInfo]:
-    html_file: str = open("antibes.html")
-    soup: BeautifulSoup = BeautifulSoup(html_file, features="html.parser")
-    all_top_restaurants: ResultSet = soup.find_all("div", {"class": "restaurants-list-ListCell__cellContainer--2mpJS"})
-    infos: List[RestaurantInfo] = [RestaurantInfo(tag) for tag in all_top_restaurants]
-    infos = [info for info in infos if not info.is_ad()]
-    for info in infos:
+def get_top_restaurants(tripadvisor_url: str, number_of_restaurants: int):
+    top_restaurants: List[RestaurantInfo] = []
+    for rank in range(0, number_of_restaurants, 30):
+        new_restaurants: List[RestaurantInfo] = get_restaurants_on_page(tripadvisor_url, rank)
+        top_restaurants += new_restaurants
+    for info in top_restaurants:
         print(f"name: {info.name}")
         print(f"url: {info.link}")
         print(f"rating: {info.rating}")
         print(f"number of reviews: {info.number_of_reviews}")
         print(f"rank: {info.rank}")
         print("")
+    return top_restaurants
+
+def get_restaurants_on_page(base_url: str, minimum_rank: int) -> List[RestaurantInfo]:
+    page_url: str = get_url_for_page(base_url, minimum_rank)
+    html_file: str = requests.get(page_url).text
+    soup: BeautifulSoup = BeautifulSoup(html_file, features="html.parser")
+    all_top_restaurants: ResultSet = soup.find_all("div", {"class": "restaurants-list-ListCell__cellContainer--2mpJS"})
+    infos: List[RestaurantInfo] = [RestaurantInfo(tag) for tag in all_top_restaurants]
+    infos = [info for info in infos if not info.is_ad()]
     return infos
 
+
+def get_url_for_page(base_url: str, minimum_rank: int) -> str:
+    location_id: str = re.findall("-g[0-9]*", base_url)[0]
+    split_by_location_id: List[str] = base_url.split(location_id)
+    new_url: str = f"{split_by_location_id[0]}{location_id}-oa{minimum_rank}{split_by_location_id[1]}"
+    return new_url
+
+
 api_key: str = sys.argv[1]
-url: str = get_top_restaurants_url("antibes", api_key)
-top_restaurants: List[RestaurantInfo] = get_top_restaurants(url)
+url: str = get_top_restaurants_url("norderstedt", api_key)
+top_restaurants: List[RestaurantInfo] = get_top_restaurants(url, 200)
