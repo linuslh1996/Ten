25da77c9721390351929d1b9ca97c1c23a86a360
Linus Heinzl <linus.heinzl@student.hpi.de>
2020-09-04 17:37:52 +0200

    adds comments

--- backend/ratings/sql.py
+++ backend/ratings/sql.py
@@ -7,7 +7,33 @@ from ratings.restaurant import *
 from ratings.database import get_column_names, DbResult
 
 
-def get_aliased_select(table: DeclarativeMeta, table_shortcut: str=None, columns: List[str] = None) -> SQL:
+def get_restaurants_without_photos(town: str) -> Composed:
+    columns = [column for column in get_column_names(GoogleMapsResult) if not column == "photos"]
+    fields_to_select = _get_aliased_select(GoogleMapsResult, columns=columns) + SQL(", ") + _get_aliased_select(TripAdvisorResult)
+    sql = SQL("SELECT {fields_to_select} FROM restaurants "
+                "INNER JOIN google_maps ON google_maps.link = restaurants.google_maps_link "
+                "INNER JOIN trip_advisor ON restaurants.trip_advisor_link  = trip_advisor.link "
+              "WHERE town = {town}")
+    return sql.format(town=Literal(town), fields_to_select=fields_to_select)
+
+
+def get_all_restaurant_info(google_maps_links: Tuple) -> Composed:
+    columns = [column for column in get_column_names(GoogleMapsResult) if not column == "photos"]
+    fields_to_select: SQL = _get_aliased_select(GoogleMapsResult, columns=columns) + SQL(", array[encode(photos[1]::bytea,'escape'), encode(photos[2]::bytea,'escape'), encode(photos[3]::bytea,'escape')] as google_maps_photos, ") + _get_aliased_select(TripAdvisorResult)
+    sql = SQL("SELECT {fields_to_select} FROM restaurants "
+              "INNER JOIN google_maps ON google_maps.link = restaurants.google_maps_link "
+              "INNER JOIN trip_advisor ON restaurants.trip_advisor_link  = trip_advisor.link "
+              "WHERE restaurants.google_maps_link IN {google_maps_links}")
+    return sql.format(fields_to_select=fields_to_select, google_maps_links=Literal(google_maps_links))
+
+def get_all_available_towns() -> SQL:
+    sql = SQL("SELECT DISTINCT town FROM restaurants")
+    return sql
+
+
+# When Performing Joins, we sometimes need to alias the columns. Especially when they contain duplicate names. This
+# method exist to do that. This is also important so that the mapping works, see the DbResult class for this.
+def _get_aliased_select(table: DeclarativeMeta, table_shortcut: str=None, columns: List[str] = None) -> SQL:
     if table_shortcut is None:
         # noinspection PyUnresolvedReferences
         table_shortcut = table.__tablename__
@@ -18,27 +44,3 @@ def get_aliased_select(table: DeclarativeMeta, table_shortcut: str=None, columns
         # noinspection PyUnresolvedReferences
         aliases.append(f"{table_shortcut}.{column} as {DbResult.get_column_alias(table.__tablename__, column)}")
     return SQL(", ".join(aliases)) + SQL(" ")
-
-
-def get_restaurants_without_photos(town: str) -> Composed:
-    columns = [column for column in get_column_names(GoogleMapsResult) if not column == "photos"]
-    fields_to_select = get_aliased_select(GoogleMapsResult, columns=columns) + SQL(", ") + get_aliased_select(TripAdvisorResult)
-    sql = SQL("SELECT {fields_to_select} FROM restaurants "
-                "INNER JOIN google_maps ON google_maps.link = restaurants.google_maps_link "
-                "INNER JOIN trip_advisor ON restaurants.trip_advisor_link  = trip_advisor.link "
-              "WHERE town = {town}")
-    return sql.format(town=Literal(town), fields_to_select=fields_to_select)
-
-
-def get_all_restaurant_info(google_maps_links: Tuple) -> Composed:
-    columns = [column for column in get_column_names(GoogleMapsResult) if not column == "photos"]
-    fields_to_select: SQL = get_aliased_select(GoogleMapsResult, columns=columns) + SQL(", array[encode(photos[1]::bytea,'escape'), encode(photos[2]::bytea,'escape'), encode(photos[3]::bytea,'escape')] as google_maps_photos, ") + get_aliased_select(TripAdvisorResult)
-    sql = SQL("SELECT {fields_to_select} FROM restaurants "
-              "INNER JOIN google_maps ON google_maps.link = restaurants.google_maps_link "
-              "INNER JOIN trip_advisor ON restaurants.trip_advisor_link  = trip_advisor.link "
-              "WHERE restaurants.google_maps_link IN {google_maps_links}")
-    return sql.format(fields_to_select=fields_to_select, google_maps_links=Literal(google_maps_links))
-
-def get_all_available_towns() -> SQL:
-    sql = SQL("SELECT DISTINCT town FROM restaurants")
-    return sql
